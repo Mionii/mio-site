634fa109b92b815c7f7636d496e8b2e90091cabb
Mionii <rtud057@hotmail.com>
2025-09-01 13:58:49 -0400

    removed image and lines

--- script.js
+++ script.js
@@ -57,8 +57,6 @@ document.addEventListener('DOMContentLoaded', () => {
                 { src: 'images/mio5.png', className: 'object-pos-custom' },
                 { src: 'images/mio6.png' },
                 { src: 'images/mio7.2.png' },
-                { src: 'images/mio8.2.png' },
-                { src: 'images/mio9.2.png' },
             ]
         }
     };
